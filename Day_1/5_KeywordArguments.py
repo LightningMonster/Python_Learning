e9e0a2dbb637e11d02428feedf1b8a9417b52a85
Yash Dhadve <lightningmonster.1548@gmail.com>
2025-05-11 23:03:20 +0530

    multiple arguments, positional arguments, keyword arguments

---
+++ Day_1/5_KeywordArguments.py
@@ -0,0 +1,11 @@
+print("My name is", "Python.", end=" ")
+print("Monty Python.")
+
+print()
+
+print("My", "name", "is", "Monty", "Python.", sep="-")
+
+print()
+
+print("My", "name", "is", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
