6f0319df15abcef88b50267c3cfd72f1bb49e225
Yash Dhadve <lightningmonster.1548@gmail.com>
2025-05-12 22:57:55 +0530

    #24 Comments: Why, When, and How?
    Signed-off-by: Yash Dhadve <lightningmonster.1548@gmail.com>

---
+++ Day_2/24_CommentsWhyWhenAndHow.py
@@ -0,0 +1,6 @@
+# This program calculates the hypotenuse of a right-angled triangle.
+a = 3.0  # First leg
+b = 4.0  # Second leg
+
+# Hypotenuse using Pythagorean theorem
+c = (a ** 2 + b ** 2) ** 0.5
