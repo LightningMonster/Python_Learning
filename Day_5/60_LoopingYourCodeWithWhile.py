151f50e949a1d815d701be2856f6032b95de7bb8
Yash Dhadve <lightningmonster.1548@gmail.com>
2025-05-15 15:45:09 +0530

    #60: 3.2.1 Looping your code with while
    Signed-off-by: Yash Dhadve <lightningmonster.1548@gmail.com>

---
+++ Day_5/60_LoopingYourCodeWithWhile.py
@@ -0,0 +1,5 @@
+number = int(input("Enter a number: "))
+while number != -1:
+    if number > 0:
+        print("Positive number entered:", number)
+    number = int(input("Enter a number or type -1 to stop: "))
